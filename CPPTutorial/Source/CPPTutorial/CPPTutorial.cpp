95add3cded766f302905f11c0a3c0dc35548aae7
corinnedaigle3 <corinnedaigle9@gmail.com>
2026-07-07 11:58:54 -0400

    old

--- CPPTutorial/Source/CPPTutorial/CPPTutorial.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPPTutorial.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPPTutorial, "CPPTutorial" );
-
-DEFINE_LOG_CATEGORY(LogCPPTutorial)
- 
--- CPPTutorial/Source/CPPTutorial/CPPTutorialGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPPTutorialGameMode.h"
-
-ACPPTutorialGameMode::ACPPTutorialGameMode()
-{
-	// stub
-}
